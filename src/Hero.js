b95f1c9520ebba6cbd8bd79b8469d33df4deb16a
Daniel <dcampoverde@ifinetwork.com>
2023-07-29 03:17:58 -0500

    design update

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,8 @@
 
 function Hero(){
+  
     return(
         <>
-        <p id="herotex">Buscando perfección?<br/> Ya sabes a donde ir</p>
         <img id="hero" alt="alarma"
         src="https://qph.fs.quoracdn.net/main-qimg-a5630af6b215a13452df3c7dd8852783"
         ></img>
